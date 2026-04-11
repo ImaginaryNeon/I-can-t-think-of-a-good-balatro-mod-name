6f009d1ba0cc76e356c8081db5dcab0a73ebc980
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-11 11:49:12 -0500

    updated mannpower blind icons

--- src/blinds/supernovablind.lua
+++ src/blinds/supernovablind.lua
@@ -10,14 +10,14 @@ SMODS.Blind {
         if context.before and not context.blueprint then
             local enhanced = {}
             for _, scored_card in ipairs(context.scoring_hand) do
-                if next(SMODS.get_enhancements(scored_card)) and not scored_card.debuff and not scored_card.vampired then
+                if next(SMODS.get_enhancements(scored_card)) and not scored_card.debuff and not scored_card.vampired and not scored_card.supernovaed then
                     enhanced[#enhanced + 1] = scored_card
-                    scored_card.vampired = true
+                    scored_card.supernovaed = true
                     scored_card:set_ability('c_base', nil, true)
                     G.E_MANAGER:add_event(Event({
                         func = function()
                             scored_card:juice_up()
-                            scored_card.vampired = nil
+                            scored_card.supernovaed = nil
                             return true
                         end
                     }))
