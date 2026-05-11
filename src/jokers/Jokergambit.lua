a41470373a0e279d9852b5e1bbd51b0d4f7101a4
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-05-11 14:45:50 -0500

    Create Jokergambit.lua

---
+++ src/jokers/Jokergambit.lua
@@ -0,0 +1,28 @@
+SMODS.Joker {
+    key = "kingambit",
+    rarity = 3,
+    cost = 8,
+    pos = { x = 3, y = 1 },
+    config = { extra = { xmult_gain = 0.1, xmult = 1 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult } }
+    end,
+        calculate = function(self, card, context)
+        if context.remove_playing_cards and not context.blueprint then
+            local cardcount = 0
+            for _, removed_card in ipairs(context.removed) do
+                cardcount = cardcount + 1 end
+            end
+            if cardcount > 0 then
+                -- See note about SMODS Scaling Manipulation on the wiki
+                card.ability.extra.xmult = card.ability.extra.xmult + cardcount * card.ability.extra.xmult_gain
+                return { message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.xmult } } }
+            end
+        end
+        if context.joker_main then
+            return {
+                xmult = card.ability.extra.xmult
+            }
+        end
+    end
+}
