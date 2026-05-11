a90f8b07d21da46d59819ef9382d776eed69a8b4
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-05-11 14:39:08 -0500

    Update and rename jokerjoyconl.lua to jokerjoycons.lua

---
+++ src/jokers/jokerjoycons.lua
@@ -0,0 +1,114 @@
+SMODS.Joker {
+    key = 'joyconl',
+    atlas = 'jonklers',
+    pos = {
+        x = 4,
+        y = 0
+    },
+    rarity = 2,
+    cost = 6,
+    config = {
+        extra = {
+            chips = 0,
+            change = 5
+        }
+    },
+    pixel_size = { w = 23, h = 47 },
+    loc_vars = function(self, info_queue, card)
+        do
+            return {
+                vars = {
+                    card.ability.extra.chips,
+                    card.ability.extra.change,
+                },
+            }
+        end
+        if card.area and card.area == G.jokers then
+            local other_joker
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] == card then other_joker = G.jokers.cards[i + 1] end
+            end
+        end
+    end,
+    calculate = function(self, card, context)
+        local other_joker = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then other_joker = G.jokers.cards[i + 1] end
+        end
+        if context.post_trigger and context.other_card == other_joker then
+            SMODS.scale_card(card, {
+                ref_table = card.ability.extra,
+                ref_value = 'chips',
+                scalar_value = 'change',
+                message_colour = G.C.ATTENTION
+            })
+        end
+        if context.joker_main then
+            return {
+                chips = card.ability.extra.chips
+            }
+        end
+    end
+}
+SMODS.Joker {
+    key = 'joyconr',
+    atlas = 'jonklers',
+    pos = {
+        x = 5,
+        y = 0
+    },
+    pixel_size = { w = 24, h = 47 },
+    rarity = 2,
+    cost = 6,
+    loc_vars = function(self, info_queue, card)
+        if card.area and card.area == G.jokers then
+            local other_joker
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i + 1] == card then other_joker = G.jokers.cards[i] end
+            end
+        end
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'neonmod_joyconr')
+        return { vars = { card.ability.extra.mult, card.ability.extra.change, numerator, denominator } }
+    end,
+    config = {
+        extra = {
+            mult = 0,
+            change = 3,
+            odds = 50,
+        }
+    },
+    block_overrides = {
+        value = true,  -- blocks modifications to the ref_value
+        scalar = true, -- blocks modifications to the scalar_value
+        message = true -- blocks modifications to the scaling_message
+    },
+
+    calculate = function(self, card, context)
+        local other_joker = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i + 1] == card then other_joker = G.jokers.cards[i] end
+        end
+        if context.post_trigger and context.other_card == other_joker then
+            SMODS.scale_card(card, {
+                ref_table = card.ability.extra,
+                ref_value = 'mult',
+                scalar_value = 'change',
+                message_colour = G.C.ATTENTION
+            })
+        end
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.mult
+
+            }
+        end
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
+            if SMODS.pseudorandom_probability(card, 'neonmod_joyconr', 1, card.ability.extra.odds) then
+                SMODS.destroy_cards(card, nil, nil, true)
+                return {
+                    message = 'Broken!'
+                }
+            end
+        end
+    end
+}
