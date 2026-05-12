c1655a3fc6594b774d55f32952fa5a1a4a3daff1
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-05-12 17:59:58 -0500

    Update jokerbase.lua

--- src/jokers/jokerbase.lua
+++ src/jokers/jokerbase.lua
@@ -58,7 +58,7 @@ SMODS.Joker {
                 local obj_key = other_joker.config.center.key
                 local obj_set = other_joker.ability.set
                 tarname = localize { type = 'name_text', set = obj_set, key = obj_key }
-                tardesc = table.concat(localize({type = 'raw_descriptions', key = key, set = 'Joker', vars = {}}), ' ')
+                tardesc = table.concat(localize({type = 'raw_descriptions', key = obj_key, set = obj_set, vars = {}}), ' ')
                 card.ability.mult = (string.len(tarname) or 0) * card.ability.multper
                 card.ability.chips = (string.len(tardesc) or 0) * card.ability.chipsper
             end
@@ -77,7 +77,7 @@ SMODS.Joker {
             local obj_key = other_joker.config.center.key
             local obj_set = other_joker.ability.set
             tarname = localize { type = 'name_text', set = obj_set, key = obj_key }
-            tardesc = table.concat(localize({type = 'raw_descriptions', key = key, set = 'Joker', vars = {}}), ' ') -- thanks eggymari
+            tardesc = table.concat(localize({type = 'raw_descriptions', key = obj_key, set = obj_set, vars = {}}), ' ') -- thanks eggymari
             card.ability.mult = (string.len(tarname) or 0) * card.ability.multper
             card.ability.chips = (string.len(tardesc) or 0) * card.ability.chipsper
         end
