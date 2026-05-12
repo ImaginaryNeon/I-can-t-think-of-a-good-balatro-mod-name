41b3798e7ba3a32c3175f3a2b54b416a4a16489c
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-05-11 19:19:12 -0500

    Update Jokergambit.lua

--- src/jokers/Jokergambit.lua
+++ src/jokers/Jokergambit.lua
@@ -7,15 +7,15 @@ SMODS.Joker {
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult } }
     end,
-        calculate = function(self, card, context)
+    calculate = function(self, card, context)
         if context.remove_playing_cards and not context.blueprint then
-            local cardcount = 0
+            local fallen_cards = 0
             for _, removed_card in ipairs(context.removed) do
-                cardcount = cardcount + 1 end
+                fallen_cards = fallen_cards + 1
             end
-            if cardcount > 0 then
+            if fallen_cards > 0 then
                 -- See note about SMODS Scaling Manipulation on the wiki
-                card.ability.extra.xmult = card.ability.extra.xmult + cardcount * card.ability.extra.xmult_gain
+                card.ability.extra.xmult = card.ability.extra.xmult + fallen_cards * card.ability.extra.xmult_gain
                 return { message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.xmult } } }
             end
         end
@@ -23,12 +23,11 @@ SMODS.Joker {
             -- See note about SMODS Scaling Manipulation on the wiki
             card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmult_gain
             return { message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.xmult } } }
-            end
         end
         if context.joker_main then
             return {
                 xmult = card.ability.extra.xmult
             }
         end
-    end
+    end,
 }
