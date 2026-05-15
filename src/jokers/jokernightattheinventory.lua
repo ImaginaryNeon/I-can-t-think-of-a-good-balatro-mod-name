c562ae3913e63f25cb304d1b0da4ecfc721485c0
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-05-15 15:17:20 -0500

    Update jokernightattheinventory.lua

--- src/jokers/jokernightattheinventory.lua
+++ src/jokers/jokernightattheinventory.lua
@@ -3,8 +3,8 @@ SMODS.Joker {
     atlas = 'jonklers',
     pos = { x = 2, y = 0 },
     config = { extra = { odds = 4 } },
-    rarity = 3,
-    cost = 7,
+    rarity = 2,
+    cost = 6,
     loc_vars = function(self, info_queue, card)
         local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds,
             'neonmod_licensetomaim')
@@ -67,4 +67,32 @@ SMODS.Joker {
             end
         end
     end
+}
+
+SMODS.Joker {
+    key = 'ironcurtain',
+    atlas = 'jonklers',
+    pos = { x = 3, y = 3 },
+    soul_pos = { x = 4, y = 3 },
+    config = { extra = { xmult = 1.25, count = 50 } },
+    rarity = 3,
+    cost = 8,
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.xmult, card.ability.extra.count } }
+    end,
+    calculate = function(self, card, context)
+        if (context.joker_main and G.GAME.blind.boss) then
+            card.ability.extra.count = card.ability.extra.count - 1
+            if card.ability.extra.count >= 0 then
+            return { xmult = card.ability.extra.xmult }
+            else
+                SMODS.destroy_cards(card, nil, nil, true)
+            end
+        end
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
+            if card.ability.extra.count <= 0 then
+                SMODS.destroy_cards(card, nil, nil, true)
+            end
+        end
+    end,
 }
